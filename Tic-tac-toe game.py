61c85a36f2fa5aa2856a2df5713971b8c112c0da
ExzorS <ilya.trohimik@gmail.com>
2022-10-20 20:46:13 +0300

    Turn order added

--- Tic-tac-toe game.py
+++ Tic-tac-toe game.py
@@ -5,20 +5,21 @@ def field():
     for i in range(3):
         print(f'{i} {playing_field[i][0]} {playing_field[i][1]} {playing_field[i][2]}')
 
-field()
-
 def player_turn():
     while True:
         cell = input("Вы ходите в: ").split()
         if len(cell) != 2:
             print('Введите 2 координаты через пробел!')
             continue
+
         x, y = cell
+
         if x.isdigit() or y.isdigit():
             x, y = map(int, cell)
         else:
             print('Введите числа!')
             continue
+
         if 0 <= x <= 2 and 0 <= y <= 2:
             if playing_field[x][y] == ' ':
                 return x, y
@@ -26,4 +27,26 @@ def player_turn():
                 print('Клетка уже занятя.')
         else:
             print('Почти попали в поле, попробуйте ещё раз =)')
-player_turn()
+
+move_number = 0
+
+while True:
+    move_number += 1
+
+    field()
+
+    if move_number % 2 == 1:
+        print('   Ходит Х')
+    else:
+        print('   Ходит О')
+
+    x, y = player_turn()
+
+    if move_number % 2 == 1:
+        playing_field[x][y] = 'X'
+    else:
+        playing_field[x][y] = 'O'
+
+    if move_number == 9:
+        break
+        print('Ничья')
